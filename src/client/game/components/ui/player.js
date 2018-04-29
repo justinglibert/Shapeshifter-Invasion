481bb8b98265fc4a310125a423214e0b3cdd9113
Robin Henry <robin.henry012@gmail.com>
2018-04-29 07:48:46 +0100

    solved conflicts

--- src/client/game/components/ui/player.js
+++ src/client/game/components/ui/player.js
@@ -3,6 +3,7 @@ import colours from './colours';
 import { Card, Elevation, Icon, Tag, Intent } from "@blueprintjs/core";
 
 
+
 class Player extends React.Component {
   renderMe() {
     if (!this.props.isMe) {
@@ -11,12 +12,19 @@ class Player extends React.Component {
 
     return <Tag intent={Intent.PRIMARY}>You!</Tag>
   }
+
   render() {
+
+    const isAlive = ({G, myID}) => {
+        return G.players[myID].alive
+    }
+
     return (
       <Card style={{ display: "flex", flexDirection: "row", justifyContent: 'space-between', alignItems: 'center', marginBottom: '1em'}} className={this.props.isDoingItsTurn && "dropshadow"}>
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
           <Icon icon="user" color={colours[this.props.player.id % 30]} />
-          <p style={{margin:'10px'}}> {this.props.player.name} </p>
+          <p> {isAlive({Game: this.props.G, myID: this.props.myID}) ? this.props.player.name : "dead " + this.props.player.name} }</p>
+          {/* <p style={{margin:'10px'}}> {this.props.player.name} </p> */}
         </div>
         { this.renderMe() }
       </Card>
